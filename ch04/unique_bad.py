4d4f1d28070ec12259eaab056212a96a95c20767
Lance Braswell <lbraswel@cisco.com>
2015-10-26 15:02:35 -0500

    ran autopep8 on the lot

--- ch04/unique_bad.py
+++ ch04/unique_bad.py
@@ -20,9 +20,16 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # WARNING: this is a terribly inefficient algorithm
+
+
 def unique3(S, start, stop):
-  """Return True if there are no duplicate elements in slice S[start:stop]."""
-  if stop - start <= 1: return True                # at most one item
-  elif not unique(S, start, stop-1): return False  # first part has duplicate
-  elif not unique(S, start+1, stop): return False  # second part has duplicate
-  else: return S[start] != S[stop-1]               # do first and last differ?
+    """Return True if there are no duplicate elements in slice S[start:stop]."""
+    if stop - start <= 1:
+        return True                # at most one item
+    elif not unique(S, start, stop - 1):
+        return False  # first part has duplicate
+    elif not unique(S, start + 1, stop):
+        return False  # second part has duplicate
+    else:
+        # do first and last differ?
+        return S[start] != S[stop - 1]
--- ch13/find_brute.py
+++ ch13/find_brute.py
@@ -19,13 +19,16 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 def find_brute(T, P):
-  """Return the lowest index of T at which substring P begins (or else -1)."""
-  n, m = len(T), len(P)                      # introduce convenient notations
-  for i in range(n-m+1):                     # try every potential starting index within T
-    k = 0                                    # an index into pattern P
-    while k < m and T[i + k] == P[k]:        # kth character of P matches
-      k += 1
-    if k == m:                               # if we reached the end of pattern,
-      return i                               # substring T[i:i+m] matches P
-  return -1                                  # failed to find a match starting with any i
+    """Return the lowest index of T at which substring P begins (or else -1)."""
+    n, m = len(T), len(P)                      # introduce convenient notations
+    # try every potential starting index within T
+    for i in range(n - m + 1):
+        k = 0                                    # an index into pattern P
+        while k < m and T[i + k] == P[k]:        # kth character of P matches
+            k += 1
+        if k == m:                               # if we reached the end of pattern,
+            # substring T[i:i+m] matches P
+            return i
+    return -1                                  # failed to find a match starting with any i
